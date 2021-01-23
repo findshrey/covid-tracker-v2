e7813327c0a5be3bb39737ea2daec37ff07cd98f
shrey9496 <shrey9april@gmail.com>
2021-01-23 22:36:09 +0530

    Minor changes

---
+++ src/components/CovidApp.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CovidApp = () => {
+   return (
+      <div>hi</div>
+   )
+}
+
+export { CovidApp as default }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import CovidApp from './components/CovidApp'
 import './styles/main.scss'
 
-const Template = () => {
-   return (
-      <div>
-         <h1>Title</h1>
-         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam, sequi?</p>
-      </div>
-   )
-}
-
-ReactDOM.render(<Template />, document.getElementById('root'))
+ReactDOM.render(<CovidApp />, document.getElementById('root'))
