61ac365c0226c9b66342a19f1c61b10b77e8443f
shrey9496 <shrey9april@gmail.com>
2021-01-26 00:14:02 +0530

    Refactor: Summary section

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -1,25 +1,25 @@
 import React from 'react'
 
 const Summary = ({ summary }) => {
-   const aa = ['active', 'deaths', 'recovered', 'confirmed']
+   const statsArray = ['confirmed', 'active', 'recovered', 'deaths']
 
    return (
       <div className="summary">
          {
-            aa.map((a) => {
-               return <SummaryBox a={a} summary={summary} />
+            statsArray.map((stat, index) => {
+               return <SummaryBox key={index} summary={summary} stat={stat} />
             })
          }
       </div>
    )
 }
 
-const SummaryBox = ({ a, summary }) => {
+const SummaryBox = ({ summary, stat }) => {
    return (
       <div className="summary-box">
-         <h1>{a}</h1>
-         <p>{summary[`delta${a}`]}</p>
-         <p>{summary[a]}</p>
+         <h4>{stat}</h4>
+         <span>{summary[`delta${stat}`]}</span>
+         <p>{summary[stat]}</p>
       </div>
    )
 }
