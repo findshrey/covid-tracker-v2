8a5b3851269dfb8e2686b7cee3f5d7949f9ba9e7
Shrey <shrey9april@gmail.com>
2021-07-30 14:44:35 +0530

    Refactor: minor changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react"
 
-import { ThemeProvider } from './../context/ThemeContext'
-import Header from './Header'
-import Home from './Home'
-import Footer from './Footer'
+import { ThemeProvider } from "../context/ThemeContext"
+import Footer from "./Footer"
+import Header from "./Header"
+import Home from "./Home"
 
 const App = () => {
    return (
